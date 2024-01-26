410705c4743d696f7c8760e3ad62af0e5edf5532
GeekMomProjects <debraansell@yahoo.com>
2024-01-25 22:29:58 -0800

    Update mic.py
    Fixed bug with microphone power not enabled

--- code/lib/mic.py
+++ code/lib/mic.py
@@ -1,6 +1,9 @@
 import audiobusio
+import digitalio
 import board
 import math
+import time
+import array
 
 
 # Remove DC bias before computing RMS.
@@ -14,17 +17,22 @@ def normalized_rms(values):
         float(sample - minbuf) * (sample - minbuf)
         for sample in values
     )
-
     return math.sqrt(samples_sum / len(values))
-class MIC(audiobusio):
-    def __init__(self, nsamples=160):
-        self.mic = audiobusio.PDMIn(board.PDM_CLK, board.PDM_DATA, frequency=16000, bit_depth=16)
-        self.nsamples=nsamples
+    
+class MIC():
+    def __init__(self):
+        self.nsamples = 160
+        micpwr = digitalio.DigitalInOut(board.MIC_PWR)
+        micpwr.direction = digitalio.Direction.OUTPUT
+        micpwr.value = True
+        time.sleep(0.1)
+
+        self.mic = audiobusio.PDMIn(board.PDM_CLK, board.PDM_DATA, bit_depth=16)
         self.samples = array.array('H',[0]*self.nsamples)
 
     # Must call this regularly to collect samples
     def record_sample(self):
-        self.record(self.samples, self.nsamples)
+        self.mic.record(self.samples, self.nsamples)
 
     def get_volume(self):
         return normalized_rms(self.samples)
